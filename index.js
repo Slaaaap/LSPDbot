a42bb974e5504fbfe35c35a81a7230c9b61c730b
Matt Grenier <mattgrenier33@gmail.com>
2020-05-26 15:08:17 +0200

    await messages ok, messages du bot pour les etapes ko

--- index.js
+++ index.js
@@ -1,158 +1,224 @@
 // Load up the discord.js library
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
+// This is your client. Some people call it `bot`, some people call it `self`,
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
+// Here we load the config.json file that contains our token and our prefix values.
 const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  console.log(`Bot is ready to operate.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`To protect and serve`);
 });
 
-client.on("guildCreate", guild => {
+client.on("guildCreate", (guild) => {
   // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  console.log(
+    `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
+  );
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
-client.on("guildDelete", guild => {
+client.on("guildDelete", (guild) => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
-
-client.on("message", async message => {
+client.on("message", async (message) => {
   // This event will run on every single message received, from any channel or DM.
-  
+
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
-  if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
+  if (message.author.bot) return;
+
+  // Also good practice to ignore any message that does not start with our prefix,
   // which is set in the configuration file.
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  // Here we separate our "command" name, and our "arguments" for the command.
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  
+
   // Let's go with a few common example commands! Feel free to delete or change those.
-  
-  if(command === "ping") {
+
+  if (command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    m.edit(
+      `Pong! Latency is ${
+        m.createdTimestamp - message.createdTimestamp
+      }ms. API Latency is ${Math.round(client.ping)}ms`
+    );
   }
-  
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
+
+  if (command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
     const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
+    message.delete().catch((O_o) => {});
+    // And we get the bot to say the thing:
     message.channel.send(sayMessage);
   }
 
-  if(command === "help") {
-    message.delete().catch(O_o=>{}); 
-    message.channel.send("`LISTE DES COMMANDES`\n`1. /help: affiche le présent message`\n`2. /purge x: nettoie x messages`");
+  if (command === "help") {
+    message.delete().catch((O_o) => {});
+    message.channel.send(
+      "`LISTE DES COMMANDES`\n`1. /help: affiche le présent message`\n`2. /purge x: nettoie x messages`"
+    );
   }
 
-  if(command === "plainte") {
-    message.delete().catch(O_o=>{});
-    const m = await message.channel.send("Processus de prise de plainte en cours. Veuillez indiquer le nom et le prénom de la victime.");
+  if (command === "plainte") {
+    message.delete().catch((O_o) => {});
 
+    // let Author = message.author;
+    // let Authorid = Author.id; //You will need this in the future
 
-    message.author.send("Processus de prise de plainte en cours. Veuillez indiquer le nom et le prénom de la victime.");
-    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-    console.log(collector)
-    collector.on('collect', message => {
-        if (message.content == "See") {
-            message.channel.send("You Want To See Someones Spec OK!");
-        } else if (message.content == "Change") {
-            message.channel.send("You Want To Change Your Spec OK!");
-        }
-    })
-  }
+    let filter = (m) => !m.author.bot;
+ 
+    message.channel.awaitMessages(filter, { max: 1 }).then((collected1) => {
   
-  if(command === "kick") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+      const response1 = collected1.first();
+      let name = response1.content;
+
+      message.channel.awaitMessages(filter, { max: 1 }).then((collected2) => {
+        
+        const response2 = collected2.first();
+        let tel = response2.content;
+
+        message.channel.send(name + " " + tel);
+      });
+    });
+
     
+    // let filter = (m) => !m.author.bot;
+    // let collector = new Discord.MessageCollector(message.channel, filter);
+    // collector.on("collect", (message, col) => {
+    //   let victimInfos = [];
+    //   if (message.author.bot) return;
+    //   if (message.content.startsWith("annuler")) {
+    //     collector.stop();
+    //     message.channel.send("Prise de plainte annulée.");
+    //   } else {
+    //     let name = message.content;
+    //     victimInfos.push(name);
+    //     message.channel
+    //       .send("```Nom de la victime : " + victimInfos[0] + "```")
+    //       .then(() => {
+    //         message.channel.send("Entrez son n° de téléphone.");
+    //         if (message.author.bot) return;
+    //         let tel = message.content;
+    //         victimInfos.push(tel);
+    //         message.channel.send("N° de téléphone : " + victimInfos[1]);
+    //       });
+    //   }
+    //   console.log("Collected message: " + message.content);
+    //   console.log("array: " + victimInfos);
+    // });
+  }
+
+  if (command === "kick") {
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit:
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if (
+      !message.member.roles.some((r) =>
+        ["Administrator", "Moderator"].includes(r.name)
+      )
+    )
+      return message.reply("Sorry, you don't have permissions to use this!");
+
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
+    let member =
+      message.mentions.members.first() || message.guild.members.get(args[0]);
+    if (!member)
       return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
+    if (!member.kickable)
+      return message.reply(
+        "I cannot kick this user! Do they have a higher role? Do I have kick permissions?"
+      );
+
     // slice(1) removes the first part, which here should be the user mention or ID
     // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No reason provided";
 
+    // Now, time for a swift kick in the nuts!
+    await member
+      .kick(reason)
+      .catch((error) =>
+        message.reply(
+          `Sorry ${message.author} I couldn't kick because of : ${error}`
+        )
+      );
+    message.reply(
+      `${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`
+    );
   }
-  
-  if(command === "ban") {
+
+  if (command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+    if (!message.member.roles.some((r) => ["Administrator"].includes(r.name)))
       return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
 
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+    let member = message.mentions.members.first();
+    if (!member)
+      return message.reply("Please mention a valid member of this server");
+    if (!member.bannable)
+      return message.reply(
+        "I cannot ban this user! Do they have a higher role? Do I have ban permissions?"
+      );
+
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No reason provided";
+
+    await member
+      .ban(reason)
+      .catch((error) =>
+        message.reply(
+          `Sorry ${message.author} I couldn't ban because of : ${error}`
+        )
+      );
+    message.reply(
+      `${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`
+    );
   }
-  
-  if(command === "purge") {
+
+  if (command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
-    
+
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
-    
+
     // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
+    if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply(
+        "Please provide a number between 2 and 100 for the number of messages to delete"
+      );
+
     // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    const fetched = await message.channel.fetchMessages({ limit: deleteCount });
+    message.channel
+      .bulkDelete(fetched)
+      .catch((error) =>
+        message.reply(`Couldn't delete messages because of: ${error}`)
+      );
   }
 });
 
